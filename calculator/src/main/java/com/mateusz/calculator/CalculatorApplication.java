954e7da7dafcbbdf85a31e2ab4e4fa09d99b3c00
Mateusz Gogloza <mateusz.go@gmail.com>
2019-09-29 16:06:41 +0200

    pierwszy commit

---
+++ calculator/src/main/java/com/mateusz/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.mateusz.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
